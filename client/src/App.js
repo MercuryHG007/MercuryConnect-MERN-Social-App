870193cbe4403f3d488804254198b6bc036cb837
Harshit Garg <ggharshit1@gmail.com>
2023-02-08 14:53:20 +0530

    Redux File Architecture and Router Setup in Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
-import './App.css';
+import { 
+  BrowserRouter,
+  Navigate,
+  Routes,
+  Route, 
+} from 'react-router-dom';
+
+import HomePage from './scenes/homePage';
+import LoginPage from './scenes/loginPage';
+import ProfilePage from './scenes/profilePage';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
