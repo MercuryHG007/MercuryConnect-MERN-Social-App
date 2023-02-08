96fffb9fb96fbef7a375b772eb98fa24d02c266f
Harshit Garg <ggharshit1@gmail.com>
2023-02-08 20:10:49 +0530

    Color, Theme and Styling Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-import { 
+import {
   BrowserRouter,
   Navigate,
   Routes,
-  Route, 
+  Route,
 } from 'react-router-dom';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
 
+import { themeSettings } from './theme';
 import HomePage from './scenes/homePage';
 import LoginPage from './scenes/loginPage';
 import ProfilePage from './scenes/profilePage';
 
 function App() {
 
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}> {/* THEME PROVIDER FOR MATERIAL UI */}
+          <CssBaseline /> {/* RESETS THE CSS TO BASIC CSS FOR MATERIAL UI */}
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
