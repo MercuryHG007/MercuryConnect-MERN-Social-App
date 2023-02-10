1c656c9ca00ff2a5650e35ae009ec3c131f19a68
Harshit Garg <ggharshit1@gmail.com>
2023-02-11 02:10:53 +0530

    Unauthorised Access Controlled for Routes

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ function App() {
 
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
@@ -26,8 +27,20 @@ function App() {
           <CssBaseline /> {/* RESETS THE CSS TO BASIC CSS FOR MATERIAL UI */}
           <Routes>
             <Route path="/" element={<LoginPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="/home" 
+              element={ 
+                isAuth 
+                  ? <HomePage />
+                  : <Navigate to="/" />
+              }
+            />
+            <Route path="/profile/:userId" 
+              element={
+                isAuth
+                  ?<ProfilePage />
+                  :<Navigate to="/" />
+              } 
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
