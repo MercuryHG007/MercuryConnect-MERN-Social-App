7e7dd00ff5baca8aa60a907726f38d362b73dd19
Harshit Garg <ggharshit1@gmail.com>
2023-02-11 02:08:39 +0530

    Register and Login Page Setup

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import {
+  Box,
+  Typography,
+  useTheme,
+  useMediaQuery
+} from '@mui/material';
+
+import Form from './form';
 
 const LoginPage = () => {
+
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  const theme = useTheme();
+  // const neutralLight = theme.palette.neutral.light;
+  // const neutralDark = theme.palette.neutral.dark;
+  // const background = theme.palette.background.default;
+  // const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
+
   return (
-    <div>LoginPage</div>
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          color="primary"
+        >
+          MercuryConnect
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={alt}
+      >
+        <Typography
+          fontWeight="500"
+          variant='h5'
+          sx={{
+            mb: "1.5rem"
+          }}
+          textAlign="center"
+        >
+          Welcome to MercuryConnect, Connecting you with friends and family.
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
   )
 }
 
