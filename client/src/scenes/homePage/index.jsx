37f8359a2ae28809ac0200d6d97e6bdacfe9e17d
Harshit Garg <ggharshit1@gmail.com>
2023-02-11 20:26:35 +0530

    Home and Widget Setup

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,13 +1,55 @@
 import {
-  Box,
+  Box, 
+  useMediaQuery,
 } from '@mui/material';
 
+import { useSelector } from 'react-redux';
+
 import Navbar from '../navbar';
+import UserWidget from '../widgets/UserWidget';
 
 const HomePage = () => {
+
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)")
+  const { _id, profileImagePath } = useSelector((state) => state.user);
+
+
   return (
     <Box>
+      
       <Navbar />
+
+      <Box
+        width="100%"
+        p="2rem 6%"
+        display={isNonMobileScreen ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box
+          flexBasis={isNonMobileScreen ? "26%" : undefined}
+        >
+          <UserWidget
+            userId={_id}
+            profileImagePath={profileImagePath}
+          />
+        </Box>
+
+        <Box
+          flexBasis={isNonMobileScreen ? "42%" : undefined}
+          mt={isNonMobileScreen ? undefined : "2rem"}
+        >
+          {/* POSTS */}
+        </Box>
+
+        {isNonMobileScreen && (
+          <Box
+            flexBasis="26%"
+          >
+            {/* SUGGESTIONS */}
+          </Box>
+        )}
+      </Box>
     </Box>
   )
 }
